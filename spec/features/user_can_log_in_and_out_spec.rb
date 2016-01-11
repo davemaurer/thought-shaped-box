4d1a7eddf6a99785d34c3b5100b3969943269d1a
David Maurer <davemaurermail@gmail.com>
2016-01-11 10:26:50 -0700

    add test for unregistered user seeing sign up option.
    create user model

---
+++ spec/features/user_can_log_in_and_out_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "User login/logout" do
+  scenario "unregistered user sees sign up option" do
+    visit root_path
+
+    expect(page).to have_content('Sign Up')
+  end
+end
