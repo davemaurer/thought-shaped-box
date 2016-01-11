036ec163d42c64c95c0a253a25edb75435f741dc
David Maurer <davemaurermail@gmail.com>
2016-01-11 10:35:51 -0700

    add site controller with show action and view. Add html to view so user sees option to sign up

--- spec/features/user_can_log_in_and_out_spec.rb
+++ spec/features/user_can_log_in_and_out_spec.rb
@@ -4,6 +4,6 @@ RSpec.feature "User login/logout" do
   scenario "unregistered user sees sign up option" do
     visit root_path
 
-    expect(page).to have_content('Sign Up')
+    expect(page).to have_content('Sign Up Here')
   end
 end
